a39af09ee96a6dc7672e6be490793e588e23967a
ShaneJasonViola <shanejasonviola99@gmail.com>
2025-08-12 01:03:41 -0400

    Update ufcstreamlit.py

--- ufcstreamlit.py
+++ ufcstreamlit.py
@@ -15,6 +15,14 @@ st.set_page_config(page_title="UFC Fight Outcome Predictor (SVM)", layout="wide"
 st.title("UFC Fight Outcome Predictor (SVM)")
 st.markdown("Enter both fighters' statistics below. The app will predict the winner using your SVM model.")
 
+# Resources
+st.markdown("## Resources to Look Up Information")
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("[Upcoming UFC Events](https://www.ufc.com/events)")
+with col2:
+    st.markdown("[UFC Fighter Statistics](http://ufcstats.com/statistics/fighters)")
+
 # ---------------------------------------------------------
 # Constants
 # ---------------------------------------------------------
